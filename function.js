16de6031027bce392a3bc412b30c2444a586f70a
munna686 <munnaispeser@gmail.com>
2022-11-22 21:15:15 +0600

    n-22/11/22

---
+++ function.js
@@ -0,0 +1,15 @@
+function sayLoveYou(){
+    console.log('jaan oh baby!!')
+    console.log('SOnar moyna pakhi')
+
+}
+sayLoveYou()
+function DateForDate(){
+    var date = 14;
+    console.log(date)
+}
+var place = 'litonFlat'
+sayLoveYou()
+var hang = 'tsc'
+sayLoveYou()
+DateForDate()
---
+++ function2.js
@@ -0,0 +1,18 @@
+function doubleIt(num){
+    var result = num * num
+    return result;
+
+}
+
+var first = doubleIt(50)
+var sec = doubleIt(100)
+var total = first + sec
+//console.log(total)
+
+
+function add(num1 , num2){
+    var result = num1 + num2;
+    return result;
+}
+var sum = add(15,17);
+console.log(sum)
