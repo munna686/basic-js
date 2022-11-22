241269135c0004ac7afebcc24e959ab37d2b7047
munna686 <munnaispeser@gmail.com>
2022-11-22 22:01:23 +0600

    n2-22/11/22

---
+++ object.js
@@ -0,0 +1,11 @@
+var student = {id: 121 , phone:145,  name:"abir" };
+var student2 = { id: 122 , phone:456 , name:"munna"};
+
+var phoneno = student.phone
+var phoneno2 = student2["phone"]
+// update phn number
+student2.phone = 654987
+student["phone"] = 258582
+student2["cinema"] = "Ogni2"
+console.log(student)
+console.log(student2)
